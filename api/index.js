2a7438f083333682cd0f76bc1a868f489ca46e75
Luis Falconi <luism.falconi@gmail.com>
2022-10-17 12:37:38 -0300

    modif

--- api/index.js
+++ api/index.js
@@ -7,8 +7,12 @@ const { conn } = require('./src/db.js');
 
 const PORT = process.env.PORT || 3001
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: false })
+  .then(() => {
   server.listen(PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
-  });
-});
+    });
+  })
+  .catch((e) => {
+    console.error(e.message);
+  })
