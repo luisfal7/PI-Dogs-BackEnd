6ebcbf7353e53db922c1bfe089155b88f022a994
Luis Falconi <luism.falconi@gmail.com>
2022-10-17 11:05:24 -0300

    modif deploy

--- api/src/db.js
+++ api/src/db.js
@@ -9,13 +9,19 @@ const {
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  host: 'localhost',
+  dialectOptions:{
+    ssl: {
+       require:true,
+       rejectUnauthorized:false
+     }                             
+    },
+  /* host: 'localhost',
   dialect: 'postgres',
   pool: {
     max: 5,
     min: 0,
     idle: 10000
-  }
+  } */
 });
 const basename = path.basename(__filename);
 
