b187988ca989c5e0c8dcdd58e51a746da884ac93
Luis Falconi <luism.falconi@gmail.com>
2022-10-18 00:00:02 -0300

    db remote

--- api/src/db.js
+++ api/src/db.js
@@ -12,7 +12,7 @@ const sequelize = new Sequelize(DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   dialectOptions:{
-    ssl: {
+    ssl:{
        require:true,
        rejectUnauthorized:false
      }                             
